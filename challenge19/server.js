543df48217e02189e2080642f5b444901536e33c
Yasa Mulky Al Afgani <mulkyasa@gmail.com>
2020-02-27 22:08:45 +0700

    server connected, data showed on table

--- challenge19/server.js
+++ challenge19/server.js
@@ -1,20 +1,32 @@
-const express = require('express')
-const app = express()
-var path = require('path');
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
+const writeData = fs.writeFileSync('data.json', JSON.stringify(data, null, 3));
+const app = express();
+var bodyParser = require('body-parser');
+
+// parse json
+app.use('/', express.static(path.join(__dirname, 'views')))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-app.get('/', function (req, res) {
-  res.render('index', {data: 'data'})
+
+app.get('/', (req, res) => {
+  res.render('index', {data:data})
 })
 
 app.get('/add', function (req, res) {
-  res.render('add', {data: 'data'})
+  res.render('add')
 })
 
 app.get('/edit', function (req, res) {
-  res.render('edit' , {data: 'data'})
+  res.render('edit')
 })
 
-app.listen(3000)
+app.listen(8000, () => {
+  console.log('This web working on port 8000')
+});

400a47da4a17c92fbf41f565daec280230361e6b
mulkyasa <mulkyasa@gmail.com>
2020-03-06 20:14:50 +0700

    edit and fiture worked properly

--- challenge21/routes/index.js
+++ challenge21/routes/index.js
@@ -24,6 +24,17 @@ module.exports = pool => {
     });
   });
 
+  router.put("/:id", function(req, res, next) {
+    const id = req.params.id;
+    const { string, integer, float, date, boolean } = req.body;
+    const sql = `UPDATE data SET string = '${string}', integer = '${integer}', float = '${float}', date = '${date}', boolean = '${boolean}' WHERE id = ${id}`;
+    pool.query(sql, (err, item) => {
+      if (err) {
+        throw err;
+      };
+      res.status(201).json({ item: item.rows });
+    });
+  });
 
   return router;
 };

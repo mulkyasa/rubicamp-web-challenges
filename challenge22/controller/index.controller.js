da641ef2726f235fb81ff2d42114796cc56fe153
mulkyasa <mulkyasa@gmail.com>
2020-03-12 09:29:44 +0700

    filter added and worked properly

--- challenge22/controller/index.controller.js
+++ challenge22/controller/index.controller.js
@@ -3,25 +3,24 @@ const dataModels = require('../models/index.models');
 moment.locale('id');
 
 const getData = (req, res) => {
-  // logic filter 
-    let input = req.query;
-    let querySearch = {}
-    // logic filter
-    if (input.check_string && input.string) {
-        querySearch.string = req.query.string
-    };
-    if (input.check_integer && input.integer) {
-      querySearch.integer = req.query.integer
-    };
-    if (input.check_float && input.float) {
-      querySearch.float = req.query.float
-    };
-    if (input.check_date) {
-      querySearch.date = {$gte: req.query.startDate, $lte: req.query.endDate}
-    };
-    if (input.check_boolean && req.query.boolean) {
-      querySearch.boolean = req.query.boolean
-    };
+  // logic filter
+  let querySearch = {}
+  // logic filter
+  if (req.query.check_string && req.query.string) {
+      querySearch.string = req.query.string
+  };
+  if (req.query.check_integer && req.query.integer) {
+    querySearch.integer = req.query.integer
+  };
+  if (req.query.check_float && req.query.float) {
+    querySearch.float = req.query.float
+  };
+  if (req.query.check_date) {
+    querySearch.date = {$gte: req.query.startDate, $lte: req.query.endDate}
+  };
+  if (req.query.check_boolean && req.query.boolean) {
+    querySearch.boolean = req.query.boolean
+  };
 
   dataModels.find(querySearch, (err, data) => {
     if (err) {
